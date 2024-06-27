f4aeefa5bf1cef6367e23d889b3fa37e817991c1
MBieganGD <mbiegan@griddynamics.com>
2024-06-27 16:42:38 +0200

    feat: removed secret.js

--- backend/secret.js
+++
@@ -1,8 +0,0 @@
-const secrets = {
-  dbUri:
-    "mongodb+srv://user1:1WkMqeI2x7gcimPu@module1project.dyv5oyv.mongodb.net/?retryWrites=true&w=majority&appName=Module1Project",
-};
-
-const getSecret = (key) => secrets[key];
-
-module.exports = getSecret;
